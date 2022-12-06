0c28307ed74a40e029b8cdddc35a4f4288655cff
Lars Pinkenburg <lars.29palms@gmail.com>
2022-12-06 15:14:18 +0100

    fix request get header and input keys not converted to lower case

--- src/Models/ApiRequest.php
+++ src/Models/ApiRequest.php
@@ -41,7 +41,8 @@ class ApiRequest
     public function input(string $key)
     {
         $inputs = array_change_key_case($this->input, CASE_LOWER);
-        return array_key_exists(strtolower($key), $inputs) ? $inputs[$key] : null;
+        $key = strtolower($key);
+        return array_key_exists($key, $inputs) ? $inputs[$key] : null;
     }
 
     /**
@@ -62,7 +63,8 @@ class ApiRequest
     public function header(string $key)
     {
         $headers = array_change_key_case($this->headers, CASE_LOWER);
-        return array_key_exists(strtolower($key), $headers) ? $headers[$key] : null;
+        $key = strtolower($key);
+        return array_key_exists($key, $headers) ? $headers[$key] : null;
     }
 
     /**
