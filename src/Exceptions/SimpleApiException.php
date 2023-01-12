61256273ae22dc76f8ecda6512efcc781702c73b
Lars Pinkenburg <lars.29palms@gmail.com>
2023-01-12 14:32:39 +0100

    add optional HTTP status code to SimpleApiException

--- src/Exceptions/SimpleApiException.php
+++ src/Exceptions/SimpleApiException.php
@@ -4,11 +4,28 @@ namespace Incapption\SimpleApi\Exceptions;
 
 use Exception;
 use Throwable;
+use Incapption\SimpleApi\Enums\HttpStatusCode;
 
 class SimpleApiException extends Exception
 {
-    public function __construct($message, $code = 0, Throwable $previous = null)
+    /**
+     * @var HttpStatusCode|null
+     */
+    private $statusCode;
+
+    public function __construct(string $message, HttpStatusCode $statusCode = null, $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
+        $this->statusCode = $statusCode;
+    }
+
+    public function hasStatusCode() : bool
+    {
+        return isset($this->statusCode);
+    }
+
+    public function getStatusCode() : ?HttpStatusCode
+    {
+        return $this->statusCode;
     }
 }
