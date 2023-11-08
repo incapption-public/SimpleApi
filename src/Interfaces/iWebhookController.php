4a322b523fea7d614336f08e814965cce8e39f17
Lars Pinkenburg <lars.29palms@gmail.com>
2023-11-08 16:42:28 +0100

    remove __construct definition from interfaces, although these are optional and not enforced, IDEs might not like it

--- src/Interfaces/iWebhookController.php
+++ src/Interfaces/iWebhookController.php
@@ -6,11 +6,5 @@ use Incapption\SimpleApi\Models\ApiRequest;
 
 interface iWebhookController
 {
-    /**
-     * Optional constructor, PHP does not enforce implementation
-     * @param ApiRequest $request
-     */
-    public function __construct(ApiRequest $request);
-
     public function handle(ApiRequest $request): iMethodResult;
 }
